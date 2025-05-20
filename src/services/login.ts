0fe7979d14f9b215f0a1f9136a0af55139661429
Amit Purohit <amitpurohit4747@gmail.com>
2025-05-21 00:02:37 +0530

    completed logn page

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,13 +1,38 @@
-import axios from "axios"
+import { userLogin } from "../api";
+import type { ApiResponse } from "../types/ApiResponse";
 
-export const handleLogin = async (username: string, password: string) => {
-    try {
-        const response = await axios.post("http://localhost:8080/api/user/auth/login", {
-            username, password
-        });
-        console.log(response.data);
-        
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const handleLogin = async (
+  username: string,
+  password: string,
+  setUsername: Function,
+  setPassword: Function,
+  setUsernameError: Function,
+  setPasswordError: Function
+) => {
+  const response: ApiResponse<string> = await userLogin(username, password);
+  if (response.status == 200) {
+    console.log("Login Success");
+    localStorage.setItem("authToken", response.data);
+    return;
+  }
+  setUsername("");
+  setPassword("");
+  if (response.status == 404) {
+    console.log("User not found");
+    setUsernameError(response.data);
+  } else if (response.status == 401) {
+    console.log("Invalid Credentials");
+    console.log(response.data);
+    
+    setPasswordError(response.data);
+    setUsernameError("Invalid Credentials");
+  } else if (response.status == 500) {
+    console.log("Server Error");
+    setUsernameError("Server Error");
+    setPasswordError("Server Error");
+  } else {
+    console.log("Unknown Error");
+    setUsernameError("Unknown Error");
+    setPasswordError("Unknown Error");
+  }
+};
